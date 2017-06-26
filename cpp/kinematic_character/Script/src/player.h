7f68c1f1d572d87c74c68bee88d75212cfca6513
Ramesh Ravone <ramesh.maran443@gmail.com>
2017-06-26 09:00:38 +0530

    Kinematic Character

---
+++ cpp/kinematic_character/Script/src/player.h
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2017 RameshRavone. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <Godot.hpp>
+#include <common.h>
+
+#include <InputEvent.hpp>
+#include <RayCast2D.hpp>
+
+#include <KinematicBody2D.hpp>
+
+NS_GODOT_BEGIN
+
+class GDPlayer : public GodotScript<KinematicBody2D> {
+	GODOT_CLASS(GDPlayer)
+
+public:
+	GDPlayer();
+	~GDPlayer();
+
+	void _init();
+	void _ready();
+
+	void _input(InputEvent *event);
+
+	void _process (const float delta);
+	void _fixed_process (const float delta);
+
+	static void _register_methods ();
+
+private:
+	bool _jumping;
+	bool _is_ready;
+	bool _prev_jump_pressed;
+
+	float _on_air_time;
+
+	Vector2 _velocity;
+	String _current_anim;
+
+	RayCast2D *ray0;
+	RayCast2D *ray1;
+
+	KinematicBody2D *_owner;
+};
+
+NS_GODOT_END
+
+#endif // PLAYER_H_
