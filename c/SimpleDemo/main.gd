55bf3afc85724ce337aa0f08ea145a5bdc3c4754
Thomas Herzog <thomas.herzog@mail.com>
2017-11-23 10:31:01 +0100

    Update main.gd

--- c/SimpleDemo/main.gd
+++ c/SimpleDemo/main.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # load the SIMPLE library
-onready var data = preload("res://bin/SIMPLE.gdns").new()
+onready var data = preload("res://bin/simple.gdns").new()
 
 func _on_Button_pressed():
 	$Label.text = "Data = " + data.get_data()
