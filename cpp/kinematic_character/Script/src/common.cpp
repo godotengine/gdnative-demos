8b3c2ee96b26b538248556cff9de5b4d2c85fe8d
karroffel <therzog@mail.de>
2018-01-18 10:37:39 +0100

    add GDN_EXPORT to C++ examples

--- cpp/kinematic_character/Script/src/common.cpp
+++ cpp/kinematic_character/Script/src/common.cpp
@@ -21,15 +21,15 @@
 
 using namespace godot;
 
-extern "C" void godot_gdnative_init(godot_gdnative_init_options *options) {
+extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *options) {
 	Godot::gdnative_init(options);
 }
 
-extern "C" void godot_gdnative_terminate(godot_gdnative_terminate_options *options) {
+extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *options) {
 	Godot::gdnative_terminate(options);
 }
 
-extern "C" void godot_nativescript_init(void *handle) {
+extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
 
 	Godot::nativescript_init(handle);
 
