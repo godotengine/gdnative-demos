9b2719045c773a9d61101cc8e8c81369faae434a
Thomas Herzog <thomas.herzog@mail.com>
2018-02-12 20:48:44 +0100

    remove accidentally pushed test code

--- c/SimpleDemo/main.gd
+++ c/SimpleDemo/main.gd
@@ -6,13 +6,3 @@ onready var data = SIMPLE.new()
 
 func _on_Button_pressed():
 	$Label.text = "Data = " + data.get_data()
-	
-	print(data.get_class())
-	print(SIMPLE.class_name)
-	
-	print("self: ", self.get_class())
-	
-	var this = get_script()
-	
-	print(self is this)
-	print(data is data.get_script())
