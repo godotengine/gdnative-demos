9f333600b6e712ac4776c0475a650237c2f49d65
lupoDharkael <izhe@hotmail.es>
2018-12-01 16:16:31 +0100

    Update cpp_constructors demo

--- cpp/cpp_constructors/Main.gd
+++ cpp/cpp_constructors/Main.gd
@@ -1,8 +1,7 @@
 extends Control
 
-onready var TestClass = preload("res://TestClass.gdn")
+onready var TestClass = preload("res://bin/testclass.gdns")
 
 func _ready():
 	var test_object = TestClass.new()
-	
 	test_object.test_method("Hello World")
