777907f6df6e2cdde243872ff7dc9a12a943eee1
didifred <freddy.boutin@gmail.com>
2020-04-25 02:44:05 +0200

    Add android build Makefile, use more generic SConstruct script ( used in godot-cpp), fix godot project

--- cpp/SimpleDemo/main.gd
+++ cpp/SimpleDemo/main.gd
@@ -4,6 +4,5 @@ const Simple = preload("res://bin/simple.gdns")
 
 onready var simple_instance = Simple.new()
 
-
 func _on_Button_pressed():
 	$Label.text = "C++ code says: " + simple_instance.get_data()
