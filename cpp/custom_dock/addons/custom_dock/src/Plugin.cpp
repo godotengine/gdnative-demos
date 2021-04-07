de3fe59e373f81f7dc8d486fbcfef58410afa965
sdtv9507 <apr8950@gmail.com>
2021-04-07 09:23:39 -0400

    Add plugin script

---
+++ cpp/custom_dock/addons/custom_dock/src/Plugin.cpp
@@ -0,0 +1,41 @@
+#include <Godot.hpp>
+#include <ResourceLoader.hpp>
+
+#include "Plugin.hpp"
+using namespace godot;
+
+Plugin::Plugin()
+{
+}
+
+Plugin::~Plugin()
+{
+}
+
+void Plugin::_init()
+{
+}
+
+void Plugin::_ready()
+{
+}
+
+void Plugin::_enter_tree()
+{
+    this->plugin_scene = ResourceLoader::get_singleton()->load("res://addons/custom_dock/Scenes/MainPanel.tscn");
+    this->plugin_instance = cast_to<Control>(this->plugin_scene->instance());
+    EditorPlugin::add_control_to_dock(0, this->plugin_instance);
+}
+
+void Plugin::_exit_tree()
+{
+    EditorPlugin::remove_control_from_docks(this->plugin_instance);
+    this->plugin_instance->free();
+}
+
+void Plugin::_register_methods() {
+	register_method((char *)"_init", &Plugin::_init);
+	register_method((char *)"_ready", &Plugin::_ready);
+	register_method((char *)"_enter_tree", &Plugin::_enter_tree);
+	register_method((char *)"_exit_tree", &Plugin::_exit_tree);
+}
---
+++ cpp/custom_dock/addons/custom_dock/src/Plugin.hpp
@@ -0,0 +1,33 @@
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+#include <Godot.hpp>
+#include <EditorPlugin.hpp>
+#include <Reference.hpp>
+#include <PackedScene.hpp>
+#include <Control.hpp>
+#include <EditorInterface.hpp>
+
+using namespace godot;
+
+class Plugin : public EditorPlugin
+{
+    GODOT_CLASS(Plugin, EditorPlugin)
+
+public:
+    Plugin();
+    ~Plugin();
+
+    void _init();
+    void _ready();
+    void _enter_tree();
+    void _exit_tree();
+	static void _register_methods();
+
+private:
+    Ref<PackedScene> plugin_scene;
+    Control *plugin_instance;
+    EditorInterface *plugin_interface;
+};
+
+#endif
