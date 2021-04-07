b4c04a214862b753601aa298629569bfdfa20567
sdtv9507 <apr8950@gmail.com>
2021-04-07 11:41:26 -0400

    Fix formatting errors

--- cpp/custom_dock/addons/custom_dock/src/Dock.cpp
+++ cpp/custom_dock/addons/custom_dock/src/Dock.cpp
@@ -1,43 +1,36 @@
 #include <Godot.hpp>
-#include <Panel.hpp>
-#include <LineEdit.hpp>
-#include <String.hpp>
 #include <Label.hpp>
+#include <LineEdit.hpp>
+#include <Panel.hpp>
+#include <String.hpp>
 
 #include "Dock.hpp"
 using namespace godot;
 
-Dock::Dock()
-{
+Dock::Dock() {
 }
 
-Dock::~Dock()
-{
+Dock::~Dock() {
 }
 
-void Dock::_init()
-{
+void Dock::_init() {
 }
 
-void Dock::_on_HelloButton_pressed()
-{
-    Godot::print("Hello World!");
+void Dock::_on_HelloButton_pressed() {
+	Godot::print("Hello World!");
 }
 
-void Dock::_on_PrintButton_pressed()
-{
-    String message = ((LineEdit *)get_node(NodePath("VBoxContainer/LineEdit")))->get_text();
-    Godot::print(message);
+void Dock::_on_PrintButton_pressed() {
+	String message = ((LineEdit *)get_node(NodePath("VBoxContainer/LineEdit")))->get_text();
+	Godot::print(message);
 }
 
-void Dock::_on_HSlider_value_changed(float value)
-{
-    ((Label *)get_node(NodePath("VBoxContainer/Tabs/Tab/PanelContainer/Label")))->set_text("Slider: "+ String::num_int64(value));
+void Dock::_on_HSlider_value_changed(float value) {
+	((Label *)get_node(NodePath("VBoxContainer/Tabs/Tab/PanelContainer/Label")))->set_text("Slider: " + String::num_int64(value));
 }
 
-void Dock::_on_ColorPickerButton_color_changed(Color color)
-{
-    ((Label *)get_node(NodePath("VBoxContainer/Tabs/Tab/PanelContainer/Label2")))->set_text("Color: r: " + String::num_int64(color.r*100) + " g: " + String::num_int64(color.g*100) + " b: " + String::num_int64(color.b*100));
+void Dock::_on_ColorPickerButton_color_changed(Color color) {
+	((Label *)get_node(NodePath("VBoxContainer/Tabs/Tab/PanelContainer/Label2")))->set_text("Color: r: " + String::num_int64(color.r * 100) + " g: " + String::num_int64(color.g * 100) + " b: " + String::num_int64(color.b * 100));
 }
 
 void Dock::_register_methods() {
