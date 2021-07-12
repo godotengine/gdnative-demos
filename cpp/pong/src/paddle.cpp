6ccec9e3ed0f18ba5c871abbe4fea3d8cb04134b
Aaron Franke <arnfranke@yahoo.com>
2021-07-12 14:01:06 -0400

    Move input and random number generator to headers and _ready()

--- cpp/pong/src/paddle.cpp
+++ cpp/pong/src/paddle.cpp
@@ -1,9 +1,9 @@
 #include "paddle.hpp"
 
-#include <Input.hpp>
-#include <RandomNumberGenerator.hpp>
-
 void Paddle::_ready() {
+	_input = godot::Input::get_singleton();
+	_random = (godot::Ref<godot::RandomNumberGenerator>)godot::RandomNumberGenerator::_new();
+	_random->randomize();
 	_screen_size_y = get_viewport_rect().size.y;
 	godot::String n = godot::String(get_name()).to_lower();
 	_up = n + "_move_up";
@@ -16,9 +16,8 @@ void Paddle::_ready() {
 }
 
 void Paddle::_process(const double delta) {
-	godot::Input *input = godot::Input::get_singleton();
 	// Move up and down based on input.
-	real_t keyboard_input = input->get_action_strength(_down) - input->get_action_strength(_up);
+	real_t keyboard_input = _input->get_action_strength(_down) - _input->get_action_strength(_up);
 	godot::Vector2 position = get_position();
 	position.y = (real_t)godot::Math::clamp(position.y + keyboard_input * MOVE_SPEED * delta, 16.0, _screen_size_y - 16.0);
 	set_position(position);
@@ -26,10 +25,8 @@ void Paddle::_process(const double delta) {
 
 void Paddle::_on_area_entered(Ball *p_ball) {
 	if (p_ball->get_name() == "Ball") {
-		godot::Ref<godot::RandomNumberGenerator> random = godot::RandomNumberGenerator::_new();
-		random->randomize();
 		// Assign new direction.
-		p_ball->direction = godot::Vector2((real_t)_ball_dir, random->randf() * 2 - 1).normalized();
+		p_ball->direction = godot::Vector2((real_t)_ball_dir, _random->randf() * 2 - 1).normalized();
 	}
 }
 
